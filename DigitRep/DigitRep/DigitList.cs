00b3884e86be8b00e83e4a3b27886c1e04fee588
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-09-03 19:06:07 -0400

    Ported some basic functionality from another project

---
+++ DigitRep/DigitRep/DigitList.cs
@@ -0,0 +1,426 @@
+﻿using Rem.Core.Attributes;
+using Rem.Core.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.Core.Math.Digits;
+
+#region Classes
+/// <summary>
+/// A wrapper for a list of <see cref="byte"/> digits.
+/// </summary>
+/// <inheritdoc cref="ULongDigitList"/>
+public sealed record class ByteDigitList(ImmutableArray<byte> Digits) : DigitList, IByteDigitList
+{
+    /// <inheritdoc/>
+    [NonNegative] public override int Count => Digits.Length;
+
+    /// <inheritdoc cref="ULongDigitList.Digits"/>
+    public ImmutableArray<byte> Digits
+    {
+        get => _digits;
+        init => Throw.IfStructPropSetDefault(in value);
+    }
+    private readonly ImmutableArray<byte> _digits = Throw.IfStructArgDefault(in Digits, nameof(Digits));
+
+    /// <inheritdoc/>
+    public byte ByteAt([NonNegative] int index) => Digits[index];
+
+    /// <inheritdoc/>
+    public ushort UShortAt([NonNegative] int index) => Digits[index];
+
+    /// <inheritdoc/>
+    public uint UIntAt([NonNegative] int index) => Digits[index];
+
+    /// <inheritdoc/>
+    public ulong ULongAt([NonNegative] int index) => Digits[index];
+
+    /// <inheritdoc/>
+    [return: NonNegative] public override BigInteger BigIntegerAt(int index) => Digits[index];
+
+    /// <summary>
+    /// A builder for a <see cref="ByteDigitList"/>.
+    /// </summary>
+    public new sealed class Builder : DigitList.Builder
+    {
+        /// <inheritdoc/>
+        public override int Count => _listBuilder.Count;
+
+        private readonly ImmutableArray<byte>.Builder _listBuilder = ImmutableArray.CreateBuilder<byte>();
+
+        /// <inheritdoc/>
+        /// <exception cref="OverflowException">
+        /// <paramref name="Digit"/> was too large for a <see cref="byte"/>.
+        /// </exception>
+        public override void Add([NonNegative] BigInteger Digit)
+        {
+            _listBuilder.Add((byte)Throw.IfArgNegative(Digit, nameof(Digit)));
+        }
+
+        private protected override DigitList ToListInternal() => ToList();
+
+        /// <inheritdoc cref="DigitList.Builder.ToList"/>
+        public new ByteDigitList ToList() => new(_listBuilder.ToImmutable());
+    }
+}
+
+/// <summary>
+/// A wrapper for a list of <see cref="ushort"/> digits.
+/// </summary>
+/// <inheritdoc cref="ULongDigitList"/>
+public sealed record class UShortDigitList(ImmutableArray<ushort> Digits) : DigitList, IUShortDigitList
+{
+    /// <inheritdoc/>
+    [NonNegative] public override int Count => Digits.Length;
+
+    /// <inheritdoc cref="ULongDigitList.Digits"/>
+    public ImmutableArray<ushort> Digits
+    {
+        get => _digits;
+        init => Throw.IfStructPropSetDefault(in value);
+    }
+    private readonly ImmutableArray<ushort> _digits = Throw.IfStructArgDefault(in Digits, nameof(Digits));
+
+    /// <inheritdoc/>
+    public ushort UShortAt([NonNegative] int index) => Digits[index];
+
+    /// <inheritdoc/>
+    public uint UIntAt([NonNegative] int index) => Digits[index];
+
+    /// <inheritdoc/>
+    public ulong ULongAt([NonNegative] int index) => Digits[index];
+
+    /// <inheritdoc/>
+    [return: NonNegative] public override BigInteger BigIntegerAt(int index) => Digits[index];
+
+    /// <summary>
+    /// A builder for a <see cref="UShortDigitList"/>.
+    /// </summary>
+    public new sealed class Builder : DigitList.Builder
+    {
+        /// <inheritdoc/>
+        public override int Count => _listBuilder.Count;
+
+        private readonly ImmutableArray<ushort>.Builder _listBuilder = ImmutableArray.CreateBuilder<ushort>();
+
+        /// <inheritdoc/>
+        /// <exception cref="OverflowException">
+        /// <paramref name="Digit"/> was too large for a <see cref="ushort"/>.
+        /// </exception>
+        public override void Add([NonNegative] BigInteger Digit)
+        {
+            _listBuilder.Add((ushort)Throw.IfArgNegative(Digit, nameof(Digit)));
+        }
+
+        private protected override DigitList ToListInternal() => ToList();
+
+        /// <inheritdoc cref="DigitList.Builder.ToList"/>
+        public new UShortDigitList ToList() => new(_listBuilder.ToImmutable());
+    }
+}
+
+/// <summary>
+/// A wrapper for a list of <see cref="uint"/> digits.
+/// </summary>
+/// <inheritdoc cref="ULongDigitList"/>
+public sealed record class UIntDigitList(ImmutableArray<uint> Digits) : DigitList, IUIntDigitList
+{
+    /// <inheritdoc/>
+    [NonNegative] public override int Count => Digits.Length;
+
+    /// <inheritdoc cref="ULongDigitList.Digits"/>
+    public ImmutableArray<uint> Digits
+    {
+        get => _digits;
+        init => Throw.IfStructPropSetDefault(in value);
+    }
+    private readonly ImmutableArray<uint> _digits = Throw.IfStructArgDefault(in Digits, nameof(Digits));
+
+    /// <inheritdoc/>
+    public uint UIntAt([NonNegative] int index) => Digits[index];
+
+    /// <inheritdoc/>
+    public ulong ULongAt([NonNegative] int index) => Digits[index];
+
+    /// <inheritdoc/>
+    [return: NonNegative] public override BigInteger BigIntegerAt(int index) => Digits[index];
+
+    /// <summary>
+    /// A builder for a <see cref="UIntDigitList"/>.
+    /// </summary>
+    public new sealed class Builder : DigitList.Builder
+    {
+        /// <inheritdoc/>
+        public override int Count => _listBuilder.Count;
+
+        private readonly ImmutableArray<uint>.Builder _listBuilder = ImmutableArray.CreateBuilder<uint>();
+
+        /// <inheritdoc/>
+        /// <exception cref="OverflowException">
+        /// <paramref name="Digit"/> was too large for a <see cref="uint"/>.
+        /// </exception>
+        public override void Add([NonNegative] BigInteger Digit)
+        {
+            _listBuilder.Add((uint)Throw.IfArgNegative(Digit, nameof(Digit)));
+        }
+
+        private protected override DigitList ToListInternal() => ToList();
+
+        /// <inheritdoc cref="DigitList.Builder.ToList"/>
+        public new UIntDigitList ToList() => new(_listBuilder.ToImmutable());
+    }
+}
+
+/// <summary>
+/// A wrapper for a list of <see cref="ulong"/> digits.
+/// </summary>
+/// <param name="Digits">The list of digits to wrap.</param>
+/// <exception cref="StructArgumentDefaultException">
+/// <paramref name="Digits"/> was default.
+/// </exception>
+public sealed record class ULongDigitList(ImmutableArray<ulong> Digits) : DigitList, IULongDigitList
+{
+    /// <inheritdoc/>
+    [NonNegative] public override int Count => Digits.Length;
+
+    /// <summary>
+    /// Gets or initializes the list of digits stored.
+    /// </summary>
+    /// <exception cref="StructPropertySetDefaultException">
+    /// This property was initialized to a default array.
+    /// </exception>
+    public ImmutableArray<ulong> Digits
+    {
+        get => _digits;
+        init => Throw.IfStructPropSetDefault(in value);
+    }
+    private readonly ImmutableArray<ulong> _digits = Throw.IfStructArgDefault(in Digits, nameof(Digits));
+
+    /// <inheritdoc/>
+    public ulong ULongAt(int index) => Digits[index];
+
+    /// <inheritdoc/>
+    [return: NonNegative] public override BigInteger BigIntegerAt(int index) => Digits[index];
+
+    /// <summary>
+    /// A builder for a <see cref="ULongDigitList"/>.
+    /// </summary>
+    public new sealed class Builder : DigitList.Builder
+    {
+        /// <inheritdoc/>
+        public override int Count => _listBuilder.Count;
+
+        private readonly ImmutableArray<ulong>.Builder _listBuilder = ImmutableArray.CreateBuilder<ulong>();
+
+        /// <inheritdoc/>
+        /// <exception cref="OverflowException">
+        /// <paramref name="Digit"/> was too large for a <see cref="ulong"/>.
+        /// </exception>
+        public override void Add([NonNegative] BigInteger Digit)
+        {
+            _listBuilder.Add((ulong)Throw.IfArgNegative(Digit, nameof(Digit)));
+        }
+
+        private protected override DigitList ToListInternal() => ToList();
+
+        /// <inheritdoc cref="DigitList.Builder.ToList"/>
+        public new ULongDigitList ToList() => new(_listBuilder.ToImmutable());
+    }
+}
+
+/// <summary>
+/// A wrapper for a list of <see cref="BigInteger"/> digits.
+/// </summary>
+/// <param name="Digits">The list of digits to wrap.</param>
+/// <exception cref="StructArgumentDefaultException">
+/// <paramref name="Digits"/> was default.
+/// </exception>
+/// <exception cref="ArgumentException">
+/// <paramref name="Digits"/> contained a negative number.
+/// </exception>
+public sealed record class BigIntegerDigitList(ImmutableArray<BigInteger> Digits) : DigitList
+{
+    /// <inheritdoc/>
+    [NonNegative] public override int Count => Digits.Length;
+
+    /// <summary>
+    /// Gets or initializes the list of digits stored.
+    /// </summary>
+    /// <exception cref="StructPropertySetDefaultException">
+    /// This property was initialized to a default array.
+    /// </exception>
+    /// <exception cref="PropertySetException">
+    /// This property was initialized to a list that contained a negative number.
+    /// </exception>
+    public ImmutableArray<BigInteger> Digits
+    {
+        get => _digits;
+        init => CheckDigitsPropertySet(in value);
+    }
+    private readonly ImmutableArray<BigInteger> _digits = CheckDigitsArgument(in Digits);
+
+    private static ImmutableArray<BigInteger> CheckDigitsArgument(in ImmutableArray<BigInteger> Digits)
+    {
+        foreach (var digit in Throw.IfStructArgDefault(in Digits, nameof(Digits)))
+        {
+            if (digit < 0) throw new ArgumentException("Cannot construct a digit list with negative digits.");
+        }
+        return Digits;
+    }
+
+    private static ImmutableArray<BigInteger> CheckDigitsPropertySet(in ImmutableArray<BigInteger> Digits)
+    {
+        foreach (var digit in Throw.IfStructPropSetDefault(in Digits, nameof(Digits)))
+        {
+            if (digit < 0) throw new PropertySetException("Cannot initialize a digit list with negative digits.");
+        }
+        return Digits;
+    }
+
+    /// <inheritdoc/>
+    [return: NonNegative] public override BigInteger BigIntegerAt(int index) => Digits[index];
+
+    /// <summary>
+    /// A builder for a <see cref="BigIntegerDigitList"/>.
+    /// </summary>
+    public new sealed class Builder : DigitList.Builder
+    {
+        /// <inheritdoc/>
+        public override int Count => _listBuilder.Count;
+
+        private readonly ImmutableArray<BigInteger>.Builder _listBuilder = ImmutableArray.CreateBuilder<BigInteger>();
+
+        /// <inheritdoc/>
+        public override void Add([NonNegative] BigInteger Digit)
+        {
+            _listBuilder.Add(Throw.IfArgNegative(Digit, nameof(Digit)));
+        }
+
+        private protected override DigitList ToListInternal() => ToList();
+
+        /// <inheritdoc cref="DigitList.Builder.ToList"/>
+        public new BigIntegerDigitList ToList() => new(_listBuilder.ToImmutable());
+    }
+}
+
+/// <summary>
+/// A wrapper for a list of digits.
+/// </summary>
+/// <remarks>
+/// This class is particularly useful in cases where a large number of digits need to be represented in an arbitrary
+/// base.
+/// If the base is small but the digit count is large, this class can save programmers lots of memory by allowing the
+/// digits to be represented as a list of <see cref="byte"/> values rather than a list of a more memory-intensive type.
+/// </remarks>
+public abstract record class DigitList : IDigitList
+{
+    /// <inheritdoc/>
+    [NonNegative] public abstract int Count { get; }
+
+    /// <summary>
+    /// Prevents this class from being extended outside of this assembly.
+    /// </summary>
+    private protected DigitList() { }
+
+    /// <inheritdoc/>
+    [return: NonNegative] public abstract BigInteger BigIntegerAt(int index);
+
+    /// <summary>
+    /// A builder for a <see cref="DigitList"/>.
+    /// </summary>
+    public abstract class Builder
+    {
+        /// <summary>
+        /// Gets the number of digits currently in the builder.
+        /// </summary>
+        public abstract int Count { get; }
+
+        /// <summary>
+        /// Prevents this class from being extended outside of this assembly.
+        /// </summary>
+        private protected Builder() { }
+
+        /// <summary>
+        /// Adds a digit to the list.
+        /// </summary>
+        /// <param name="Digit"></param>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="Digit"/> was negative.</exception>
+        public abstract void Add([NonNegative] BigInteger Digit);
+
+        /// <summary>
+        /// Gets the <see cref="DigitList"/> represented by the current state of the builder.
+        /// </summary>
+        /// <returns></returns>
+        public DigitList ToList() => ToListInternal();
+
+        /// <summary>
+        /// Allows the <see cref="ToList"/> method to be hidden in derived classes so that the return type matches
+        /// the type of list being built.
+        /// </summary>
+        /// <returns></returns>
+        private protected abstract DigitList ToListInternal();
+    }
+}
+#endregion
+
+#region Interfaces
+/// <summary>
+/// An interface for types wrapping a list of <see cref="byte"/> digits.
+/// </summary>
+public interface IByteDigitList : IUShortDigitList
+{
+    /// <inheritdoc cref="IDigitList.BigIntegerAt(int)"/>
+    public byte ByteAt([NonNegative] int index);
+}
+
+/// <summary>
+/// An interface for types wrapping a list of <see cref="ushort"/> digits.
+/// </summary>
+public interface IUShortDigitList : IUIntDigitList
+{
+    /// <inheritdoc cref="IDigitList.BigIntegerAt(int)"/>
+    public ushort UShortAt([NonNegative] int index);
+}
+
+/// <summary>
+/// An interface for types wrapping a list of <see cref="uint"/> digits.
+/// </summary>
+public interface IUIntDigitList : IULongDigitList
+{
+    /// <inheritdoc cref="IDigitList.BigIntegerAt(int)"/>
+    public uint UIntAt([NonNegative] int index);
+}
+
+/// <summary>
+/// An interface for types wrapping a list of <see cref="ulong"/> digits.
+/// </summary>
+public interface IULongDigitList : IDigitList
+{
+    /// <inheritdoc cref="IDigitList.BigIntegerAt(int)"/>
+    public ulong ULongAt([NonNegative] int index);
+}
+
+/// <summary>
+/// An interface for types wrapping a list of digits.
+/// </summary>
+public interface IDigitList
+{
+    /// <summary>
+    /// Gets the number of digits in the list.
+    /// </summary>
+    [NonNegative] public int Count { get; }
+
+    /// <summary>
+    /// Gets the digit at the specified index.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="index"/> was out of range.</exception>
+    [return: NonNegative] public BigInteger BigIntegerAt([NonNegative] int index);
+}
+#endregion
---
+++ DigitRep/DigitRep/Digits.cs
@@ -0,0 +1,136 @@
+﻿using Rem.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.Core.Math.Digits;
+
+/// <summary>
+/// Static functionality relating to digits as supported by this library.
+/// </summary>
+public static class Digits
+{
+    /// <summary>
+    /// An empty digit list.
+    /// </summary>
+    public static readonly DigitList EmptyList = new ByteDigitList(ImmutableArray<byte>.Empty);
+
+    /// <summary>
+    /// Gets a list of digits representing the given <see cref="BigInteger"/> value in the given base.
+    /// </summary>
+    /// <param name="value">The value to represent.</param>
+    /// <param name="base">The base of the representation to create.</param>
+    /// <param name="includeZeroIfZero">
+    /// Whether or not to return a digit list containing a single zero digit if <paramref name="value"/> is zero.
+    /// <para/>
+    /// If this parameter is <see langword="false"/>, the resulting digit list will be empty if
+    /// <paramref name="value"/> is zero.
+    /// </param>
+    /// <remarks>
+    /// The digit list returned when <paramref name="value"/> is zero will be empty.
+    /// </remarks>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// <paramref name="value"/> was negative
+    /// <para/>
+    /// OR
+    /// <para/>
+    /// <paramref name="base"/> was less than or equal to 1.
+    /// </exception>
+    public static DigitList GetList([NonNegative] BigInteger value, BigInteger @base, bool includeZeroIfZero = true)
+    {
+        // Validate
+        Throw.IfArgNegative(value, nameof(value));
+        ValidateBaseArg(@base, nameof(@base));
+
+        // Build digit list
+        var builder = SmallestRange(@base).CreateListBuilder();
+        while (!value.IsZero)
+        {
+            value = BigInteger.DivRem(value, @base, out var remainder);
+            builder.Add(remainder);
+        }
+        if (includeZeroIfZero && builder.Count == 0) builder.Add(0);
+        return builder.ToList();
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="DigitList.Builder"/> capable of constructing a list of digits in the current range.
+    /// </summary>
+    /// <param name="range"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidEnumArgumentException">The current range was an unnamed enum value.</exception>
+    public static DigitList.Builder CreateListBuilder(this Range range)
+        => Throw.IfEnumArgUnnamed(range, nameof(range)) switch
+        {
+            Range.Byte => new ByteDigitList.Builder(),
+            Range.UShort => new UShortDigitList.Builder(),
+            Range.UInt => new UIntDigitList.Builder(),
+            Range.ULong => new ULongDigitList.Builder(),
+            _ => new BigIntegerDigitList.Builder(),
+        };
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal static BigInteger ValidateBaseArg(BigInteger @base, string argName)
+        => Throw.IfArgLessThanOrEqualTo(1, @base, argName);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal static BigInteger InitializeBase(
+        ref BigInteger field, BigInteger @base, [CallerMemberName] string? propertyName = null)
+        => field = Throw.IfPropSetLessThanOrEqualTo(1, @base, propertyName);
+
+    /// <summary>
+    /// Gets the smallest range capable of representing digits in the given base.
+    /// </summary>
+    /// <param name="Base"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="Base"/> was less than or equal to 1.</exception>
+    [return: NameableEnum]
+    public static Range SmallestRange(BigInteger Base)
+    {
+        Throw.IfArgLessThanOrEqualTo(1, Base, nameof(Base));
+
+        if (Base <= byte.MaxValue) return Range.Byte;
+        if (Base <= ushort.MaxValue) return Range.UShort;
+        if (Base <= uint.MaxValue) return Range.UInt;
+        if (Base <= ulong.MaxValue) return Range.ULong;
+        return Range.BigInteger;
+    }
+
+    /// <summary>
+    /// Represents the ranges of digits supported by this library.
+    /// </summary>
+    public enum Range : byte
+    {
+        /// <summary>
+        /// Represents a digit that fits into a <see cref="byte"/>.
+        /// </summary>
+        Byte,
+
+        /// <summary>
+        /// Represents a digit that fits into a <see cref="ushort"/>.
+        /// </summary>
+        UShort,
+
+        /// <summary>
+        /// Represents a digit that fits into a <see cref="uint"/>.
+        /// </summary>
+        UInt,
+
+        /// <summary>
+        /// Represents a digit that fits into a <see cref="ulong"/>.
+        /// </summary>
+        ULong,
+
+        /// <summary>
+        /// Represents a digit that fits into a (non-negative) <see cref="System.Numerics.BigInteger"/>.
+        /// </summary>
+        BigInteger,
+    }
+}
+
